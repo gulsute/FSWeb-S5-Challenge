862e49b47c83874d6e69c8a8f3a65b4f8cc1064e
gulsute <134259376+gulsute@users.noreply.github.com>
2023-06-24 11:20:30 +0300

    tablar eklendi

--- src/bileşenler/tablar.js
+++ src/bileşenler/tablar.js
@@ -1,28 +1,55 @@
+import axios from "axios";
+
 const Tablar = (konu) => {
-  // GÖREV 3
-  // ---------------------
-  // Tek argümanı bir dizi ("konu") olan bu fonksiyonu uygulayın.
-  // Örnek olarak, konu dizisi şu şekilde deklare edilmişse ['javascript', 'bootstrap', 'teknoloji']
-  // fonksiyon aşağıdaki şekilde bir DOM öğesi döndürecek.
-  // Kullanılan etiketler, öğelerin hiyerarşisi ve öznitelikleri sağlanan işaretlemeyle eşleşmelidir!
-  // Öğelerin içindeki metin, "textContent" özelliği kullanılarak ayarlanacaktır ("innerText" DEĞİL).
-  //
-  // <div class="topics">
-  //   <div class="tab">javascript</div>
-  //   <div class="tab">bootstrap</div>
-  //   <div class="tab">teknoloji</div>
-  // </div>
-  //
-}
+  const topics = document.createElement("div");
+  topics.classList.add("topics");
+
+  konu.map((item) => {
+    const tab = document.createElement("div");
+    tab.classList.add("tab");
+    tab.textContent = item;
+    topics.appendChild(tab);
+  });
+  return topics;
+};
+
+// GÖREV 3
+// ---------------------
+// Tek argümanı bir dizi ("konu") olan bu fonksiyonu uygulayın.
+// Örnek olarak, konu dizisi şu şekilde deklare edilmişse ['javascript', 'bootstrap', 'teknoloji']
+// fonksiyon aşağıdaki şekilde bir DOM öğesi döndürecek.
+// Kullanılan etiketler, öğelerin hiyerarşisi ve öznitelikleri sağlanan işaretlemeyle eşleşmelidir!
+// Öğelerin içindeki metin, "textContent" özelliği kullanılarak ayarlanacaktır ("innerText" DEĞİL).
+//
+// <div class="topics">
+//   <div class="tab">javascript</div>
+//   <div class="tab">bootstrap</div>
+//   <div class="tab">teknoloji</div>
+// </div>
+//
 
 const tabEkleyici = (secici) => {
-  // GÖREV 4
-  // ---------------------
-  // Tek argümanı olarak bir css seçici alan bu işlevi uygulayın.
-  // Konuları bu uç noktadan almalıdır: `http://localhost:5001/api/konular` (console.log ile test edin!).
-  // Yanıtın içindeki konu dizisini bulun ve Tablar bileşenini kullanarak tabları oluşturun.
-  // Tabları, fonksiyona iletilen seçiciyle eşleşen DOM'daki öğeye ekleyin.
-  //
-}
+  const tabContainer = document.querySelector(secici);
 
-export { Tablar, tabEkleyici }
+  axios
+    .get("http://locallhost:5001/api/konular")
+    .then(function (response) {
+      const tabbox = Tablar(response.data.konular);
+      tabContainer.appendChild(tabbox);
+      console.log(response.data);
+    })
+    .catch(function (error) {
+      tabContainer.textContent = "Bir bağlantı hatası oluştu";
+      console.log(error);
+    });
+};
+
+export { Tablar, tabEkleyici };
+
+// GÖREV 4
+// ---------------------
+// Tek argümanı olarak bir css seçici alan bu işlevi uygulayın.
+// Konuları bu uç noktadan almalıdır: `http://localhost:5001/api/konular` (console.log ile test edin!).
+// Yanıtın içindeki konu dizisini bulun ve Tablar bileşenini kullanarak tabları oluşturun.
+// Tabları, fonksiyona iletilen seçiciyle eşleşen DOM'daki öğeye ekleyin.
+//
